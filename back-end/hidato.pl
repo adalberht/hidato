b09b8e8da6412395b714764c5958e21e201db291
Usama <usamazub@gmail.com>
2019-05-02 10:45:07 +0700

    add solved/0

--- back-end/hidato.pl
+++ back-end/hidato.pl
@@ -2,6 +2,8 @@
 :- use_module(validator, [valid/2, grid/2]).
 :- use_module(storage, [memo/3, ans/3, reader/0]).
 
+:- dynamic solved/1.
+
 remove(40) :- !.
 
 remove(Now) :-
@@ -58,9 +60,11 @@ iterate(R_now, C_now, Val_now) :-
 
 find_ans :-
     ans(R, C, 1),
-    iterate(R, C, 1), !.
+    iterate(R, C, 1), !,
+    assert(solved(1)).
 
 
 clear :-
+    retractall(solved(_)),
     retractall(memo(_, _, _)),
     retractall(ans(_, _, _)).
