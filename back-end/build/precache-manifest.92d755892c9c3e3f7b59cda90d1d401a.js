cd56c00f09a30e4ec4940eb4417571c990f9b127
Albertus Angga <albertus.angga@ui.ac.id>
2019-05-02 23:49:27 +0700

    Add deployment to heroku config

---
+++ back-end/build/precache-manifest.92d755892c9c3e3f7b59cda90d1d401a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde4b41535c456dd9ba64fc61ffd83e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a6bd9f489fdfe3f66f0",
+    "url": "/static/css/main.5a9c79a7.chunk.css"
+  },
+  {
+    "revision": "d4b621244c3ec409b383",
+    "url": "/static/js/2.4ec49237.chunk.js"
+  },
+  {
+    "revision": "9a6bd9f489fdfe3f66f0",
+    "url": "/static/js/main.95c49e1e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
