c227dda9116342106a9f6110f8467757eaff6324
Usama <usamazub@gmail.com>
2019-05-02 10:59:50 +0700

    add filled/3

--- back-end/hidato.pl
+++ back-end/hidato.pl
@@ -1,6 +1,6 @@
 :- use_module(movement, [move/2]).
 :- use_module(validator, [valid/2, grid/2]).
-:- use_module(storage, [memo/3, ans/3, reader/0]).
+:- use_module(storage, [memo/3, ans/3, filled/3, reader/0]).
 
 :- dynamic solved/0.
 
--- back-end/storage.pl
+++ back-end/storage.pl
@@ -1,25 +1,25 @@
 :- module(storage, 
-    [memo/3, ans/3, reader/0]).
-
+    [memo/3, ans/3, filled/3, reader/0]).
 
 :- dynamic memo/3.
 :- dynamic ans/3.
+:- dynamic filled/3.
 
 % belum pasti
 reader :-
-    assert(ans(1, 1, 38)),
-    assert(ans(1, 3, 40)),
-    assert(ans(1, 5, 6)),
-    assert(ans(2, 2, 39)),
-    assert(ans(2, 4, 7)),
-    assert(ans(3, 3, 11)),
-    assert(ans(4, 2, 1)),
-    assert(ans(4, 4, 9)),
-    assert(ans(5, 6, 17)),
-    assert(ans(6, 3, 31)),
-    assert(ans(6, 4, 27)),
-    assert(ans(7, 5, 25)),
-    assert(ans(7, 6, 23)),
-    assert(ans(7, 7, 22)),
-    assert(ans(8, 8, 21)),
-    assert(ans(9, 9, 10)).
+    assert(filled(1, 1, 38)),
+    assert(filled(1, 3, 40)),
+    assert(filled(1, 5, 6)),
+    assert(filled(2, 2, 39)),
+    assert(filled(2, 4, 7)),
+    assert(filled(3, 3, 11)),
+    assert(filled(4, 2, 1)),
+    assert(filled(4, 4, 9)),
+    assert(filled(5, 6, 17)),
+    assert(filled(6, 3, 31)),
+    assert(filled(6, 4, 27)),
+    assert(filled(7, 5, 25)),
+    assert(filled(7, 6, 23)),
+    assert(filled(7, 7, 22)),
+    assert(filled(8, 8, 21)),
+    assert(filled(9, 9, 10)).
